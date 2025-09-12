414c5a85b23d3fe9be978f5f250520edc8d63d53
Martin Erren <martin3r@Mac.fritz.box>
2025-09-12 18:20:06 +0200

    dashboard init

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -36,7 +36,7 @@
         
         <x-ui-dashboard-tile
             title="Monatliche Kosten"
-            :count="$monthlyTotal < 1 ? number_format($monthlyTotal, 2, ',', '.') . ' €' : number_format($monthlyTotal, 0, ',', '.') . ' €'"
+            :count="$monthlyTotal"
             subtitle="Aktueller Monat"
             icon="banknotes"
             variant="info"
