c0901cdd4b31ce2e3c98a1b288649b30547d0efc
Martin Erren <martin3r@Mac.fritz.box>
2025-10-03 18:49:57 +0200

    modal modules

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,54 +36,13 @@
    
     <!-- Grid-Icon über der Sidebar -->
     <div class="fixed top-0 left-0 z-50" style="top: 50px;">
-        <div x-data="{ showDropdown: false }" class="relative">
-            <button 
-                @click="$dispatch('toggle-sidebar')" 
-                class="w-14 h-14 d-flex items-center justify-center bg-white border border-muted hover:bg-gray-50 transition"
-                title="Sidebar umschalten"
-            >
-                @svg('heroicon-o-squares-2x2', 'w-5 h-5 text-primary')
-            </button>
-            
-            <!-- Quick Actions Dropdown -->
-            <div 
-                x-show="showDropdown" 
-                @click.away="showDropdown = false"
-                x-transition
-                class="absolute top-full left-0 mt-1 w-48 bg-white border border-muted rounded shadow-lg z-50"
-            >
-                <div class="p-2">
-                    <button 
-                        @click="$dispatch('open-modal-modules', { tab: 'modules' }); showDropdown = false"
-                        class="w-full text-left px-3 py-2 hover:bg-gray-50 rounded text-sm"
-                    >
-                        @svg('heroicon-o-cube', 'w-4 h-4 inline mr-2') Module
-                    </button>
-                    <button 
-                        @click="$dispatch('open-modal-modules', { tab: 'team' }); showDropdown = false"
-                        class="w-full text-left px-3 py-2 hover:bg-gray-50 rounded text-sm"
-                    >
-                        @svg('heroicon-o-users', 'w-4 h-4 inline mr-2') Team
-                    </button>
-                    <button 
-                        @click="$dispatch('open-modal-modules', { tab: 'account' }); showDropdown = false"
-                        class="w-full text-left px-3 py-2 hover:bg-gray-50 rounded text-sm"
-                    >
-                        @svg('heroicon-o-user', 'w-4 h-4 inline mr-2') Konto
-                    </button>
-                </div>
-            </div>
-            
-            <!-- Right-click für Dropdown -->
-            <button 
-                @contextmenu.prevent="showDropdown = !showDropdown"
-                @click="$dispatch('open-modal-modules', { tab: 'modules' })"
-                class="w-14 h-14 d-flex items-center justify-center bg-white border border-muted hover:bg-gray-50 transition"
-                title="Module öffnen (Rechtsklick für Menü)"
-            >
-                @svg('heroicon-o-squares-2x2', 'w-5 h-5 text-primary')
-            </button>
-        </div>
+        <button 
+            @click="$dispatch('toggle-sidebar')" 
+            class="w-14 h-14 d-flex items-center justify-center bg-black border border-gray-700 hover:bg-gray-800 transition"
+            title="Sidebar umschalten"
+        >
+            @svg('heroicon-o-squares-2x2', 'w-5 h-5 text-white')
+        </button>
     </div>
 
     <x-ui-sidebar>
