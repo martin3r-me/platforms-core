46a51c6474a7a55879f54de048f5f8a38f88bac4
Martin Erren <martin3r@Mac.fritz.box>
2025-10-10 10:42:51 +0200

    command for seeders

--- resources/views/layouts/embedded.blade.php
+++ resources/views/layouts/embedded.blade.php
@@ -10,12 +10,15 @@
   <x-ui-styles />
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   @livewireStyles
+  <style>
+    html, body { height: 100%; }
+  </style>
 </head>
 
-<body class="bg-[var(--ui-body-bg)] text-[var(--ui-body-color)] overflow-hidden">
-  <div class="flex h-screen w-full">
-    <main class="flex-1 min-w-0 h-screen bg-white flex flex-col overflow-hidden">
-      <div class="flex-1 min-h-0 overflow-y-auto">
+<body class="bg-[var(--ui-body-bg)] text-[var(--ui-body-color)] h-full overflow-hidden">
+  <div class="flex h-full w-full">
+    <main class="flex-1 min-w-0 h-full bg-white flex flex-col overflow-hidden">
+      <div class="flex-1 min-h-0 h-full overflow-y-auto">
         @yield('content')
       </div>
     </main>
