b1c079aa775dca25889679801176bc9601587876
Martin Erren <martin3r@Mac.fritz.box>
2025-10-11 23:48:12 +0200

    command for seeders

--- resources/views/livewire/modal-checkin.blade.php
+++ resources/views/livewire/modal-checkin.blade.php
@@ -251,7 +251,7 @@
                             name="newTodoTitle"
                             x-model="todoTitle"
                             placeholder="Neue Aufgabe hinzufügen..."
-                            class="flex-1"
+                            class="flex-1 w-full"
                             @keydown.enter="$wire.set('newTodoTitle', todoTitle); $wire.addTodo(); todoTitle = '';"
                         />
                         <x-ui-button wire:click="addTodo" variant="primary" iconOnly>
