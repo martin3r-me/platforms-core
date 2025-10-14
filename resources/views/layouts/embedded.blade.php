b2d7cdf4ad39f64dc8cbcfd2057d97ffd8f081d5
Martin Erren <martin3r@Mac.fritz.box>
2025-10-14 09:27:04 +0200

    command for seeders

--- resources/views/layouts/embedded.blade.php
+++ resources/views/layouts/embedded.blade.php
@@ -5,6 +5,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <script>
+    window.__laravelAuthed = {{ auth()->check() ? 'true' : 'false' }};
+  </script>
 
   <title>{{ config('app.name', 'Platform') }}</title>
 
