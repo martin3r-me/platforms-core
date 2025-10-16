8ebc366b98b5e6f7993f9d57cdbedde4220b2ea2
Martin Erren <martin3r@Mac.fritz.box>
2025-10-16 15:14:18 +0200

    command for seeders

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,7 @@ Route::get('/embedded/config', function () {
     ]);
     $response->headers->set('Content-Security-Policy', "frame-ancestors https://*.teams.microsoft.com https://teams.microsoft.com https://*.skype.com");
     return $response;
-})->name('embedded.config');
+})->withoutMiddleware([\Illuminate\Http\Middleware\FrameGuard::class])->name('embedded.config');
 
 Route::get('/embedded/config/okrs', function () {
     // Platzhalterseite – später: OKR-Auswahl (Teams-Tab-Konfiguration)
