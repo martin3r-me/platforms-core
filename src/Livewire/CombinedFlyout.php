c067e1476db39bc4edd88b06b651bfbcdc2dc95b
Martin Erren <martin3r@Mac.fritz.box>
2025-10-18 22:54:57 +0200

    team invitations

--- src/Livewire/CombinedFlyout.php
+++ src/Livewire/CombinedFlyout.php
@@ -12,11 +12,13 @@ class CombinedFlyout extends Component
     public $userTeams;
     public $currentTeam;
     public $modules;
+    public $currentModule;
 
     public function mount()
     {
         $this->loadTeams();
         $this->loadModules();
+        $this->loadCurrentModule();
     }
 
     public function loadTeams()
@@ -48,6 +50,23 @@ class CombinedFlyout extends Component
 
             return $userAllowed || $teamAllowed;
         })->take(4)->values();
+    }
+
+    public function loadCurrentModule()
+    {
+        $currentPath = request()->segment(1);
+        
+        if ($currentPath === 'dashboard' || empty($currentPath)) {
+            $this->currentModule = 'Dashboard';
+        } else {
+            $moduleModel = \Platform\Core\Models\Module::where('key', $currentPath)->first();
+            if ($moduleModel) {
+                $config = json_decode($moduleModel->config, true);
+                $this->currentModule = $config['title'] ?? ucfirst($currentPath);
+            } else {
+                $this->currentModule = ucfirst($currentPath);
+            }
+        }
     }
 
     public function switchTeam(int $teamId)
